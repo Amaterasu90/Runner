3adeae53962738cf0f1240f0d39c38aaf1dbfb62
Ciastek90 <arekpoly@gmail.com>
2016-01-07 16:07:22 +0100

    Add OrbitalActor. Delete Floateing Actor.
    OrbitalActor
    Alfa - Rotation Angle XY Plane (in start Z Axis) <0,360>
    Beta - Connected with Alfa Angle (if Alfa equals of more than 360 then
    beta is decresed). Rotation angle YZ Plane (X' Axis)
    Gamma - Rotation angle XY Plane (Z' Axis)(one degree of freedom)

---
+++ Source/Runner/Private/OrbitalActor.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Runner.h"
+#include "OrbitalActor.h"
+
+
+// Sets default values
+AOrbitalActor::AOrbitalActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SetMobility(EComponentMobility::Movable);
+	GetStaticMeshComponent()->SetStaticMesh(mesh);
+}
+
+// Called when the game starts or when spawned
+void AOrbitalActor::BeginPlay()
+{
+	StartPosition = GetActorLocation();
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AOrbitalActor::Tick( float DeltaTime )
+{
+	FVector NewLoaction = StartPosition;
+
+	float x = NewLoaction.X;
+	float y = NewLoaction.Y;
+	float z = NewLoaction.Z;
+	
+	float dX = Radius * (FMath::Sin(ChangeToRadians(Alfa))*FMath::Sin(ChangeToRadians(Gamma)) - FMath::Cos(ChangeToRadians(Alfa))*FMath::Cos(ChangeToRadians(Beta))*FMath::Sin(ChangeToRadians(Gamma))+ FMath::Sin(ChangeToRadians(Beta))*FMath::Sin(ChangeToRadians(Gamma)));
+	float dY = Radius * (FMath::Sin(ChangeToRadians(Alfa))*FMath::Sin(ChangeToRadians(Gamma)) + FMath::Cos(ChangeToRadians(Alfa))*FMath::Cos(ChangeToRadians(Beta))*FMath::Cos(ChangeToRadians(Gamma)) - FMath::Sin(ChangeToRadians(Beta))*FMath::Cos(ChangeToRadians(Gamma)));
+	float dZ = Radius * (FMath::Cos(ChangeToRadians(Beta)) + FMath::Cos(ChangeToRadians(Alfa))*FMath::Sin(ChangeToRadians(Beta)));
+
+	Alfa = Mod(Alfa + DeltaTime * SpeedFactor,360.0f);
+
+	if (Alfa + DeltaTime * SpeedFactor > 360.0f){
+		Beta = Mod(Beta + 3, 360.0f);
+		Alfa = 0;
+	}
+
+	NewLoaction.X = x + dX;
+	NewLoaction.Y = y + dY;
+	NewLoaction.Z = z + dZ;
+	SetActorLocation(NewLoaction);
+
+	Super::Tick(DeltaTime);
+}
+
+float AOrbitalActor::ChangeToRadians(float degrees)
+{
+	return degrees * PI / 180.0f;
+}
+
+float AOrbitalActor::Mod(float data, float boundary)
+{
+	if (boundary > 0 && data / boundary > 1)
+		return data - boundary;
+	else
+		return data;
+}
+
---
+++ Source/Runner/Public/OrbitalActor.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Runtime/Engine/Classes/Engine/StaticMeshActor.h"
+#include "OrbitalActor.generated.h"
+
+UCLASS()
+class RUNNER_API AOrbitalActor : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+public:
+	FVector StartPosition;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float Alfa;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float Beta;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float Gamma = 90.0f;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float Radius = 20.0f;
+	UPROPERTY(EditAnywhere, Category = "Rotation")
+	float SpeedFactor = 250.0f;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "View")
+	UStaticMesh * mesh;
+	UStaticMeshComponent * staticMeshComponent;
+
+
+	// Sets default values for this actor's properties
+	AOrbitalActor();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+private:
+	float ChangeToRadians(float randians);
+	float Mod(float data, float boundary);
+};
