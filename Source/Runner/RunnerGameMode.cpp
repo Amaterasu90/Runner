a99c2f374bda41681ccbe1aa4200aaea40ead21f
Ciastek90 <arekpoly@gmail.com>
2016-01-21 14:15:46 +0100

    Add functionallyty change and add menu to existing Viewport

--- Source/Runner/RunnerGameMode.cpp
+++ Source/Runner/RunnerGameMode.cpp
@@ -3,6 +3,34 @@
 #include "Runner.h"
 #include "RunnerGameMode.h"
 
+void ARunnerGameMode::BeginPlay() {
+	Super::BeginPlay();
+	ChangeMenuWidget(StartingWidgetClass);
+}
 
+void ARunnerGameMode::ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass) {
+	if (CurrentWidget != nullptr) {
+		CurrentWidget->RemoveFromViewport();
+		CurrentWidget = nullptr;
+	}
+	if (NewWidgetClass != nullptr) {
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (CurrentWidget != nullptr) {
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
 
+void ARunnerGameMode::AddMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass) {
+	if (NewWidget != nullptr) {
+		NewWidget->RemoveFromViewport();
+		NewWidget = nullptr;
+	}
 
+	if (NewWidgetClass != nullptr) {
+		NewWidget = CreateWidget<UUserWidget>(GetWorld(), NewWidgetClass);
+		if (NewWidget != nullptr) {
+			NewWidget->AddToViewport();
+		}
+	}
+}
--- Source/Runner/RunnerGameMode.h
+++ Source/Runner/RunnerGameMode.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "GameFramework/GameMode.h"
+#include "Blueprint/UserWidget.h"
 #include "RunnerGameMode.generated.h"
 
 /**
@@ -12,8 +13,19 @@ UCLASS()
 class RUNNER_API ARunnerGameMode : public AGameMode
 {
 	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable, Category = "UMG Game")
+	void ChangeMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+	UFUNCTION(BlueprintCallable, Category = "UMG Game")
+	void AddMenuWidget(TSubclassOf<UUserWidget> NewWidgetClass);
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "UMG Game")
+	TSubclassOf<UUserWidget> StartingWidgetClass;
 	
+	UPROPERTY()
+	UUserWidget* CurrentWidget;
 	
-	
-	
+	UUserWidget* NewWidget;
 };
